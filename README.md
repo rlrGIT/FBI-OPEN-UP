a516061791db31249a284a190dd39efe289acaaf
rlrGIT <44305717+rlrGIT@users.noreply.github.com>
2024-04-19 23:56:37 -0400

    Notes to self about how to dump syscall table addr
    	* Includes README formatting changes
    	* Adds resource notes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FBI-OPEN-UP
-A basic Linux Kernel rootkit
+A basic Linux Kernel rootkit targeting aarch64.
+
+### Architecture Resources:
+
+http://www.ouah.org/LKM_HACKING.html#I.1
+
+https://people.kernel.org/linusw/
+
+https://people.kernel.org/linusw/how-the-arm32-kernel-starts
+
+https://krinkinmu.github.io/2024/01/14/aarch64-virtual-memory.html
+
+https://stackoverflow.com/questions/45216054/arm64-linux-memory-write-protection-wont-disable
+
+### Source code:
+
+https://github.com/torvalds/linux/blob/master/arch/arm64/mm/mmu.c
+
--- fbi.c
+++ fbi.c
@@ -6,9 +6,39 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kprobes.h>
+#include <linux/kallsyms.h>
+
+static struct kprobe kp  = {
+	.symbol_name = "kallsyms_lookup_name"
+};
+
+typedef unsigned long (*syscall_addr_t)(const char *name);
+
+void kprobe_addr(void) {
+	syscall_addr_t call_ptr;
+	register_kprobe(&kp);
+
+	call_ptr = (syscall_addr_t) kp.addr; // not sure what this is doing
+
+	/* instead of using the directly exposed "kallsyms_lookup_name(const char*)"
+	 * Can we create a kprobe to look for it and then call it indirectly?
+	 *
+	 * Alternatively, if we know the relative offset of a syscall in the table,
+	 * is there anything that stops us from kprobing a syscall, finding its 
+	 * address in memory (where it was called from within the calltable), and 
+	 * then subtracting/adding offset to find the pointer to the head of the 
+	 * array?
+	 *
+	 * A lot more reading will need to be done before code is written.
+	 */
+	printk("Address of lookup function? at %d", *kp.addr);
+	unregister_kprobe(&kp);
+}
 
 static int startup(void) {
 	printk(KERN_NOTICE "FBI, OPEN UP!");
+	kprobe_addr();
 	return 0;
 }
 
@@ -16,6 +46,7 @@ static void shutdown(void) {
 	printk(KERN_NOTICE "Closing...");
 }
 
+/* These are static */
 module_init(startup);
 module_exit(shutdown);
 MODULE_LICENSE("GPL");
