4ac23cdca46fc59604d1f2f784edb8e96dfd9443
rlrGIT <44305717+rlrGIT@users.noreply.github.com>
2024-04-21 03:36:30 -0400

    Update code to dump an address of kernel symbols
    	* REQUIRED - need to compile .config with
    	KALLSYMS=y; this option allows the kernel to
    	maintain a list of symbols.
    
    	* TODO: recompile Kernel with new .config
    	* Fixes segfault in issue - forgot to check
    	for null pointers
    	* TODO: add hooks for rootkit

--- fbi.c
+++ fbi.c
@@ -6,44 +6,30 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/kprobes.h>
 #include <linux/kallsyms.h>
 
-static struct kprobe kp  = {
-	.symbol_name = "kallsyms_lookup_name"
-};
-
-typedef unsigned long (*syscall_addr_t)(const char *name);
-
-void kprobe_addr(void) {
-	syscall_addr_t call_ptr;
-	register_kprobe(&kp);
-
-	call_ptr = (syscall_addr_t) kp.addr; // not sure what this is doing
-
-	/* instead of using the directly exposed "kallsyms_lookup_name(const char*)"
-	 * Can we create a kprobe to look for it and then call it indirectly?
-	 *
-	 * Alternatively, if we know the relative offset of a syscall in the table,
-	 * is there anything that stops us from kprobing a syscall, finding its 
-	 * address in memory (where it was called from within the calltable), and 
-	 * then subtracting/adding offset to find the pointer to the head of the 
-	 * array?
-	 *
-	 * A lot more reading will need to be done before code is written.
-	 */
-	printk("Address of lookup function? at %d", *kp.addr);
-	unregister_kprobe(&kp);
+static unsigned long *get_table_addr(void) {
+	// does the custom kernel need to be compiled with "KALLSYMS=y?"
+	// ANSWER: yes - this needs to be enabled otherwise this is always null
+	return (unsigned long *) kallsyms_lookup_name("sys_call_table");
 }
 
+typedef unsigned long *kspace_addr_t;
+
 static int startup(void) {
+	kspace_addr_t sys_call_addr;
+	sys_call_addr = get_table_addr();
+
 	printk(KERN_NOTICE "FBI, OPEN UP!");
-	kprobe_addr();
+	if (sys_call_addr) {
+		printk("Dumped address of call table at: %lu\n", *sys_call_addr);
+	}
+	printk("kallsyms_lookup_name returned null pointer.");
 	return 0;
 }
 
 static void shutdown(void) {
-	printk(KERN_NOTICE "Closing...");
+	printk(KERN_NOTICE "Closing...\n");
 }
 
 /* These are static */
