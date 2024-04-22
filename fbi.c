52c30f1a6ddd97af57cd6e516bc158b72987d065
rlrGIT <44305717+rlrGIT@users.noreply.github.com>
2024-04-22 05:30:06 -0400

    Use kprobes for address dump, add kernel configs
    	* Remove old config.sh - it does not account for new
    	make overwriting previous choices when invoked if there
    	are new options to choose from. Use `make defconfig` and
    	edit with `make menuconfig`. You can use default kernel
    	config as a reference for options.
    
    	* Add reference and custom .config files - allows debugging
    	reference when comparing how broken and working kernels are
    	compiled.
    
    	* Add kprobe-based address dumping for kernel symbols -
    	Before kernel versions 5.x.x, kallsyms_lookup_name was
    	exposed in headers under GPL license. For later kernel
    	versions, if KALLSYMS=y and KPROBES=y, kprobes can be
    	used to dump the address kallsyms_lookup_name.
    	If KALLSYMS_ALL=y, then you are able to find the
    	`sys_call_table` symbol by passing it to
    	kallsyms_lookup_name.
    
    	* Add preproc macro for exposed kallsyms_lookup_name

--- fbi.c
+++ fbi.c
@@ -1,38 +1,71 @@
 /*
- * Boilerplate and makefile from: Daniel Graham
+ * Exposed symbol boilerplate and makefile from: Daniel Graham
  * Ethical Hacking - A Hands-On Introduction to Breaking In
  *
- */
+ * https://docs.kernel.org/trace/kprobes.html
+ */ 
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/kallsyms.h>
+#include <linux/kprobes.h>
 
-static unsigned long *get_table_addr(void) {
-	/*
-	 * Requires:
-	 *
-	 * - Custom kernel with exposed method `kallsyms_lookup_name()`
-	 *   https://github.com/rlrGIT/linux/tree/v6.1-exposed-symbols
-	 *
-	 * - Kernel compiled with KALLSYMS=y an KALLSYMS_ALL=y in 
-	 *   .config file. This allows the kernel to track and 
-	 *   store symbols like 'sys_call_table'.
-	 */
-	return (unsigned long *) kallsyms_lookup_name("sys_call_table");
+#define PROC_FAIL 1;
+
+int lookup_prehandler(struct kprobe *probe, struct pt_regs *regs) {
+	printk(KERN_NOTICE "Attempting kprobe address dump...");
+	return 0;
 }
 
-typedef unsigned long *kspace_addr_t;
+typedef unsigned long (*pt_kallsyms_lookup_name)(const char *);
+
+unsigned long find_ksymbol(const char *name) {
+/*
+ * These preprocessor macros are kinda cool,
+ * but they don't seem scalable. This works...
+ * for now.
+ */
+#ifdef kallsyms_lookup_name
+	printk("Found exposed symbol kallsyms_lookup_name.");
+	return kallsyms_lookup_name(name);
+#endif
+#ifdef CONFIG_KPROBES
+	pt_kallsyms_lookup_name lookup;
+
+	struct kprobe probe = {
+		.symbol_name = "kallsyms_lookup_name",
+		.pre_handler = lookup_prehandler,
+	};
+	
+	if (register_kprobe(&probe) != 0) {
+		printk(KERN_ERR "Registering kallsyms probe failed.");
+		return -PROC_FAIL;
+	}
+
+	lookup = (pt_kallsyms_lookup_name) probe.addr;
+	unregister_kprobe(&probe);
+	printk(KERN_NOTICE "Got kallsyms_lookup_name address: %lu.",
+		(unsigned long) lookup);
+
+	return lookup(name);
+#endif
+	// TODO: add linear address search from through exposed symbols?
+	// Add some functionality if kprobes aren't available
+	return -PROC_FAIL;
+}
+
+// TODO: should cache the table address somehow when we start using it
+// to grab syscalls
 
 static int startup(void) {
-	kspace_addr_t sys_call_addr;
-	sys_call_addr = get_table_addr();
-
-	printk(KERN_NOTICE "FBI, OPEN UP!");
-	if (sys_call_addr) {
-		printk("Dumped address of call table at: %lu\n", *sys_call_addr);
+	unsigned long sys_call_table_addr;
+	sys_call_table_addr = find_ksymbol("sys_call_table");
+	if (sys_call_table_addr <= 0) {
+		printk(KERN_ERR "Could not find symbol.");
+	} else {
+		printk(KERN_NOTICE "Dumped call table at: %lu.\n",
+			(unsigned long) sys_call_table_addr);
 	}
-	printk("kallsyms_lookup_name returned null pointer.");
 	return 0;
 }
 
@@ -40,7 +73,6 @@ static void shutdown(void) {
 	printk(KERN_NOTICE "Closing...\n");
 }
 
-/* These are static */
 module_init(startup);
 module_exit(shutdown);
 MODULE_LICENSE("GPL");
